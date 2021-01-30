8279ceda34e8e0b50e0e00eb58429474e3993494
Marina <43283310+codeoutback@users.noreply.github.com>
2021-01-29 22:21:58 -0600

    contact button

---
+++ contact_button.js
@@ -0,0 +1,25 @@
+'use strict';
+
+const e2 = React.createElement;
+
+class ContactButton extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+//If the about me button is clicked, take me to aboutMe.html page
+  render() {
+    if (this.state.liked) {
+      location.href="contact.html";
+    }
+
+    return e(
+      'button',
+      { onClick: () => this.setState({ liked: true }) },
+      'Contact Me'
+    );
+  }
+}
+const domContainer = document.querySelector('#contact_button_container');
+ReactDOM.render(e(ContactButton), domContainer);
