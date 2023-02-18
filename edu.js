acba36443d849dc0357e1574217d41a8f641072f
Marina Yancey <43283310+codeoutback@users.noreply.github.com>
2023-02-18 17:01:30 -0500

    Update edu.js

--- edu.js
+++ edu.js
@@ -1,19 +1,6 @@
-class ListEdu extends React.Component {
+class Welcome extends React.Component {
     render() {
-      return (
-        <div className="education-list">
-          <h1>My Education{this.props.name}</h1>
-          <ul>
-            <li>Associates</li>
-            <li>Bachelors</li>
-          </ul>
-        </div>
-      );
+      return <h1>Hello, {this.props.name}</h1>;
     }
   }
-
-  return React.createElement('div', {className: 'education-list'},
-  React.createElement('h1', /* ... h1 children ... */),
-  React.createElement('ul', /* ... ul children ... */)
-);
   
