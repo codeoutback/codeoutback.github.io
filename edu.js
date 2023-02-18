a90f954ed490e9a73fc00ed0193541691f868aa2
Marina Yancey <43283310+codeoutback@users.noreply.github.com>
2023-02-18 17:37:36 -0500

    performing a for loop

---
+++ edu.js
@@ -0,0 +1,20 @@
+//function displayEdu
+function displayEdu(degree) {      
+
+    for(let i=0; i<degree.length; i++){
+
+
+    }
+    
+}
+      
+
+
+
+
+
+// Calling a function
+
+
+
+add(degree);
