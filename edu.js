3641a1f8df282306439e2b67692e6c515d4d42f7
Marina Yancey <43283310+codeoutback@users.noreply.github.com>
2023-02-19 08:43:33 -0500

    large update

--- edu.js
+++ edu.js
@@ -1,25 +1,26 @@
-//function displayEdu
-function displayEdu(degree) {    
-    
-    let section = "";
+function myAssoc() {
+    document.getElementById("assocDrpDwn").classList.toggle("show");
+  }
+  
+  function myBach() {
+    document.getElementById("bachDrpDwn").classList.toggle("show");
+  }
+  
+  function certs() {
+    document.getElementById("certDrpDwn").classList.toggle("show");
+  }
 
-    for(let i=0; i<degree.length; i++){
 
-        
-
+  window.onclick = function(event) {
+    if (!event.target.matches('.expand')) {
+      var dropdowns = document.getElementsByClassName("drpDwn-content");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDrpdwn = dropdowns[i];
+        if (openDrpdwn.classList.contains('show')) {
+          openDrpdwn.classList.remove('show');
+        }
+      }
     }
-
-    return section;
-    
-}
-      
-
-
-
-
-
-// Calling a function
-
-
-
-add(degree);
+  }
+  
