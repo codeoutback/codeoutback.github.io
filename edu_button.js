751bb9e5665120c9b07a6c2eb31e4afdee68b211
Marina Yancey <43283310+codeoutback@users.noreply.github.com>
2023-02-21 11:50:47 -0500

    added notes

--- edu_button.js
+++ edu_button.js
@@ -1,15 +1,17 @@
 'use strict';
 
+//This is creating a React element named e1 and it is of constant type  
 const e1 = React.createElement;
 
-
+//This creates a react componnent named EduButton and it inherits the React.Component named e1 that is the parent and the EduButton is the child
 class EduButton extends React.Component {
   constructor(props) {
     super(props);
     this.state = { liked: false };
   }
 
-//If the about me button is clicked, take me to aboutMe.html page
+/*If the about me button is clicked, take me to aboutMe.html page. The above component starts the state as false which is liked. Then 
+the below code assigns the html file and the onClick makes it so if clicked, go to that page*/
   render() {
     if (this.state.liked) {
       location.href="edu.html";
@@ -22,5 +24,10 @@ class EduButton extends React.Component {
     );
   }
 }
+
+//This makes it so domContainer1 is assigned to the edution button
 const domContainer1 = document.querySelector('#edu_button_container');
+
+/*This is the final piece. The ReactDom takes 2 arguments the e1(EduButton) and the domContainer1. This is saying whats gonna happen and where
+is it going to happen.*/
 ReactDOM.render(e1(EduButton), domContainer1);
